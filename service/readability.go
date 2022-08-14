6d2e1e0b11836644ab483b1d55a9e96ead5140ce
jimxl <tianxiaxl@gmail.com>
2022-08-15 02:27:10 +0800

    文章读取错误的时候，把url显示出来

--- service/readability.go
+++ service/readability.go
@@ -9,7 +9,7 @@ import (
 func FetchWebContent(url string) (excerpt, image string, err error) {
 	article, err := readability.FromURL(url, 10*time.Second)
 	if err != nil {
-		fmt.Printf("文章读取错误: %s", err)
+		fmt.Printf("文章读取错误: %s\n url is [%s]\n", err, url)
 		return "", "", err
 	}
 	return article.TextContent, article.Image, nil
