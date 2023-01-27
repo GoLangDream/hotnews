9c7cf9fcc04627027f31786b7aad94a0f19a563d
jimxl <tianxiaxl@gmail.com>
2023-01-27 14:35:57 +0800

    修改了启动脚本方便添加到系统服务使用

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-cat application.pid  | xargs kill -9
-rm application.log
+#cat application.pid  | xargs kill -9
+#rm application.log
 git pull
 go build
 export ICEBERG_ENV=production
 iceberg db:migrate
-nohup ./hot_news > application.log 2>&1 &
+./hot_news
